f634a6a32009dd7329c1767e306c8f2f4ec215e8
Wagner Patrick de Carvalho <wagncarv@gmail.com>
2021-06-20 23:42:32 -0300

    Update imc.ex

--- lib/wabanex/imc.ex
+++ lib/wabanex/imc.ex
@@ -2,5 +2,9 @@ defmodule Wabanex.IMC do
 
     def calculate(filename) do
         File.read(filename)
+        |> handle_file()
     end
+
+    defp handle_file({:ok, content}), do: content
+    defp handle_file({:error, reason}), do: {:error, reason}
 end
