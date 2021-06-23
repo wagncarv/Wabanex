cba062e42eb2e687b4ea67aab0dc21c00aab6f19
Wagner Patrick de Carvalho <wagncarv@gmail.com>
2021-06-22 22:59:00 -0300

    mutation

--- lib/wabanex_web/resolvers/user.ex
+++ lib/wabanex_web/resolvers/user.ex
@@ -1,5 +1,7 @@
 defmodule WabanexWeb.Resolvers.User do
-    alias Wabanex.Users.Get
+    alias Wabanex.Users.{Create, Get}
+
+    def create(%{input: params}, _context), do: Create.call(params)
 
     def get(%{id: user_id}, _context), do: Get.call(user_id)
 end
